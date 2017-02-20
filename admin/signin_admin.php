09ff0f16b4a144b251d908e5e32e26b1251f1feb
Unknown <millkamill@hotmail.com>
2017-02-20 18:16:13 +0100

    add database

--- admin/signin_admin.php
+++ admin/signin_admin.php
@@ -3,20 +3,6 @@ require_once "database_admin.php";
 $db = new database();
 
 
-if($_POST){
-    $user = $db->getUserByEmail($_POST["email"]);
-    if(empty($user)){
-        echo "Wrong user";
-    }elseif (password_verify($_POST["password"], $user["password"])){
-        session_start();
-        $_SESSION["userid"] = $user["id"];
-        header("Location: admin.php");
-    }else{
-        echo "Wrong password";
-    }
-}
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -30,6 +16,20 @@ if($_POST){
         <h1>My Language School</h1>
         <h2>Admin site</h2>
     </header>
+    <?php
+    if($_POST){
+    $user = $db->getUserByEmail($_POST["email"]);
+    if(empty($user)){
+        echo "<h1>Wrong user! - Try again! </h1><br>";
+    }elseif (password_verify($_POST["password"], $user["password"])){
+        session_start();
+        $_SESSION["userid"] = $user["id"];
+        header("Location: admin.php");
+    }else{
+        echo "<h1>Wrong password! - Try again!</h1><br>";
+    }
+}
+?>
     <h4>Since it is a demo site you can register yourself or use the following data to log in</h4>
     <p>Email: admin1@admin.org</p>
     <p>Password: admin1</p>
@@ -39,6 +39,7 @@ if($_POST){
     Password:<br>
     <input type="password" name="password"><br>
     <input type="submit" value="Log in" class="button">
+
 </form>
 </body>
 </html>
